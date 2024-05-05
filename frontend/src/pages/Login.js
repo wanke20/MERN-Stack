b8909b711ef835e971e134723d139f0c78f14d8d
wanke20 <122842952+wanke20@users.noreply.github.com>
2024-05-04 23:24:28 -0400

    add auth context

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,33 +1,36 @@
 import { useState } from "react"
+import { useLogin } from "../hooks/useLogin"
 
 const Login = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const { login, error, isLoading } = useLogin()
 
     const handleSubmit = async (e) => {
         e.preventDefault()
 
-        console.log(email, password)
+        await login(email, password)
     }
 
     return (
         <form className="login" onSubmit={handleSubmit}>
             <h3>Log In</h3>
 
-            <label>Email:</label>
-            <input 
+            <label>Email address:</label>
+            <input
                 type="email"
                 onChange={(e) => setEmail(e.target.value)}
-                value={email} 
+                value={email}
             />
             <label>Password:</label>
-            <input 
+            <input
                 type="password"
                 onChange={(e) => setPassword(e.target.value)}
-                value={password} 
+                value={password}
             />
 
-            <button>Log in</button>
+            <button disabled={isLoading}>Log in</button>
+            {error && <div className="error">{error}</div>}
         </form>
     )
 }
